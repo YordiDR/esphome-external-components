5039f8be55b69fb12a3f28d77ffa9a3ce5f58ac9
YordiDR <yordi.derijcke@live.be>
2026-03-31 09:53:08 +0200

    aa55_bus: Add temp logging for troubleshooting

--- esphome/components/aa55_bus/aa55_bus.cpp
+++ esphome/components/aa55_bus/aa55_bus.cpp
@@ -189,6 +189,11 @@ void AA55Bus::process_rx() {
     ESP_LOGD(LOGGING_TAG, "Received packet from a registered inverter (%x).", packet_source_address);
 
     // Pass packet to inverter object
+    ESP_LOGD(LOGGING_TAG,
+             "Payload that bus will parse: %d bytes, first byte is received_payload[6] (%x), last byte is "
+             "received_payload[%d] (%x)",
+             this->receive_buffer_.at(6), this->receive_buffer_.at(6), 6 + this->receive_buffer_.at(6) - 1,
+             this->receive_buffer_.at(6 + this->receive_buffer_.at(6) - 1));
     const std::vector<uint8_t> received_payload(this->receive_buffer_.begin() + 6,
                                                 this->receive_buffer_.begin() + 6 + this->receive_buffer_.at(6));
     const aa55_const::AA55Packet response_packet = {packet_source_address, this->receive_buffer_.at(3),
